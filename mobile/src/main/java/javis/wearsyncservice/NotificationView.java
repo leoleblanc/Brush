bbacdbb862633d8801565ec118cbef9340641e3e
IamKorra13 <a.collins05@berkeley.edu>
2016-04-27 10:08:44 -0700

    Fixed errors

--- mobile/src/main/java/javis/wearsyncservice/NotificationView.java
+++ mobile/src/main/java/javis/wearsyncservice/NotificationView.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class NotificationView extends AppCompatActivity {
@@ -14,8 +13,8 @@ public class NotificationView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_view);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
