78196a1afbfdce1f49a482574732c0dab620e9a9
Serah Isaac <serah.isaactho@berkeley.edu>
2016-04-29 12:23:14 -0700

    Registration Screens Template

---
+++ mobile/src/main/java/javis/wearsyncservice/Registration2Activity.java
@@ -0,0 +1,65 @@
+package javis.wearsyncservice;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileInputStream;
+
+public class Registration2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration2);
+
+        Bitmap bmp = getImageBitmap(this, "profile","BMP");
+        ImageView img = (ImageView) findViewById(R.id.cropped_final);
+        img.setImageDrawable(new RoundedAvatarDrawable(bmp));
+    }
+
+    public void do_photo_selection_2(View w)
+    /**
+     * Called onClick of the purple photo button. Brings up the
+     * photo selection dialog
+     */
+    {
+        Intent intent = new Intent(this, CircCropActivity.class);
+        startActivityForResult(intent, CircCropImplementation.BITMAP_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bmp = getImageBitmap(this, "profile","BMP");
+        ImageView img = (ImageView) findViewById(R.id.cropped_final);
+        img.setImageDrawable(new RoundedAvatarDrawable(bmp));
+        /*switch(requestCode) {
+            case (MY_CHILD_ACTIVITY) : {
+                if (resultCode == Activity.RESULT_OK) {
+                    // TODO Extract the data returned from the child Activity.
+                }
+                break;
+            }
+        }*/
+    }
+
+
+    public Bitmap getImageBitmap(Context context,String name,String extension){
+        name=name+"."+extension;
+        try{
+            FileInputStream fis = context.openFileInput(name);
+            Bitmap b = BitmapFactory.decodeStream(fis);
+            fis.close();
+            return b;
+        }
+        catch(Exception e){
+        }
+        return null;
+    }
+}
